3ecb4bb8de53b86332e687ebf7156e2c4e08f8a8
Kyle Oliveira <kyle.oliveira@cornell.edu>
2013-12-10 11:49:59 -0500

    Added a login_as action to the LoginPage class that allows a bit of simplification of the preexisting login steps. This also allows for implementation of the "I am backdoored as..." step because in the Cornell KFS implementation, the base user login method is used for backdoor-ing.

--- features/step_definitions/chart_of_accounts/login_to_KFS.rb
+++ features/step_definitions/chart_of_accounts/login_to_KFS.rb
@@ -1,13 +1,11 @@
 Given /^I am logged in as a KFS Fiscal Officer$/ do
-  visit LoginPage do |page|
-    page.username.set 'jguillor'
-    page.login
-  end
+  visit(LoginPage).login_as('jguillor')
 end
 
 Given(/^I am logged in as a KFS User$/) do
-  visit LoginPage do |page|
-    page.username.set 'khuntley'
-    page.login
-  end
+  visit(LoginPage).login_as('khuntley')
+end
+
+Given /^I am backdoored as "([^"]*)"$/ do |user_id|
+  visit(LoginPage).login_as(user_id)
 end
