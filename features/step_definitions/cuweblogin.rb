7d22eb9fcac5c6f3bf50cf316418d14b5073f0fd
Kyle Oliveira <kyle.oliveira@cornell.edu>
2013-12-11 10:57:24 -0500

    Added an if statement that will skip the CUWebLogin procedure if the CUWebLogin page doesn't actually pop up. This allows you to test against the KFS TestDrive or our CI environment without changing your configuration, in addition to getting rid of the error you would run into previously when running multiple Scenarios in the same Cucumber run with the CUWebLogin in the Background steps. With this, I think we're ready to merge the feature branch into Master.

--- features/step_definitions/cuweblogin.rb
+++ features/step_definitions/cuweblogin.rb
@@ -1,8 +1,13 @@
 Given /^I am logged in via CUWebLogin$/ do
   @config = YAML.load_file("#{File.dirname(__FILE__)}/../support/config.yml")[:basic]
   visit CUWebLoginPage do |page|
-    page.netid.set @config[:cuweblogin_user]
-    page.password.set @config[:cuweblogin_password]
-    page.login
+    if page.page_header_element.exists?
+      page.netid.set @config[:cuweblogin_user]
+      page.password.set @config[:cuweblogin_password]
+      page.login
+    end
+    # Otherwise, we're already logged in (or not actually using CUWebLogin)
+    # and we can safely continue on our way. Something else should break if
+    # anything is amiss.
   end
 end
--- lib/kuality-kfs/page_objects/cuweblogin_page.rb
+++ lib/kuality-kfs/page_objects/cuweblogin_page.rb
@@ -1,9 +1,10 @@
 class CUWebLoginPage < BasePage
   page_url "#{$base_url}portal.do"
 
-  element(:netid) { |b| b.text_field(name: 'netid')}
-  element(:password) { |b| b.text_field(name: 'password')}
-  value(:page_header) { |b| b.h1(text: 'CUWebLogin')}
+  element(:netid) { |b| b.text_field(name: 'netid') }
+  element(:password) { |b| b.text_field(name: 'password') }
+  element(:page_header_element) { |b| b.h1(text: 'CUWebLogin') }
+  value(:page_header) { |b| b.page_header_element.text }
   button 'Login'
 
 end
