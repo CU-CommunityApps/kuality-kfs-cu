e4f62567f38cfad7a2b4fd9a7d398955a27114e4
Kyle Oliveira <kyle.oliveira@cornell.edu>
2013-12-13 15:16:18 -0500

    Fixed a typo.

--- features/step_definitions/chart_of_accounts/account.rb
+++ features/step_definitions/chart_of_accounts/account.rb
@@ -15,7 +15,7 @@ When(/^I create an account with blank SubFund group Code$/) do
   @account = create AccountObject, sub_fnd_group_cd: ''
 end
 
-Then(/^I should get an error on saving that I left teh SubFund Group Code field blank$/) do
+Then(/^I should get an error on saving that I left the SubFund Group Code field blank$/) do
 #  on AccountPage do |page|
 #    page.errors.should include 'Sub-Fund Group Code (SubFundGrpCd) is a required field.'
   on(AccountPage).errors.should include 'Sub-Fund Group Code (SubFundGrpCd) is a required field.'
