85cc2b6ac3f7fe988d5296fd228ce29ccfb1efb6
Kyle Oliveira <kyle.oliveira@cornell.edu>
2013-12-17 15:44:26 -0500

    Reformulated the require-er to better handle symlinks.

--- lib/kuality-kfs-cu.rb
+++ lib/kuality-kfs-cu.rb
@@ -1,7 +1,8 @@
 require 'kuality-kfs'
 
-Dir["#{File.dirname(__FILE__)}/kuality-kfs-cu/*.rb"].alphabetize.each {|f| require f }
-Dir["#{File.dirname(__FILE__)}/kuality-kfs-cu/page_objects/*.rb"].alphabetize.each {|f| require f }
-Dir["#{File.dirname(__FILE__)}/kuality-kfs-cu/page_objects/*/*.rb"].alphabetize.each {|f| require f }
-Dir["#{File.dirname(__FILE__)}/kuality-kfs-cu/data_objects/*.rb"].alphabetize.each {|f| require f }
-Dir["#{File.dirname(__FILE__)}/kuality-kfs-cu/data_objects/*/*.rb"].alphabetize.each {|f| require f }
+cwd = File.symlink?(__FILE__) ? File.dirname(File.readlink(__FILE__)) : File.dirname(__FILE__)
+Dir["#{cwd}/kuality-kfs-cu/*.rb"].alphabetize.each {|f| require f }
+Dir["#{cwd}/kuality-kfs-cu/page_objects/*.rb"].alphabetize.each {|f| require f }
+Dir["#{cwd}/kuality-kfs-cu/page_objects/*/*.rb"].alphabetize.each {|f| require f }
+Dir["#{cwd}/kuality-kfs-cu/data_objects/*.rb"].alphabetize.each {|f| require f }
+Dir["#{cwd}/kuality-kfs-cu/data_objects/*/*.rb"].alphabetize.each {|f| require f }
