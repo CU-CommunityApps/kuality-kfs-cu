9890c6f05c1ef048ad5213fa858634faf15ba28e
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-01-07 09:45:53 -0500

    [KFSQA-567] Moved the @wip tag to the right feature and fixed some small style issues. I think it's ready to be merged to master.

--- features/step_definitions/chart_of_accounts/account_delegate.rb
+++ features/step_definitions/chart_of_accounts/account_delegate.rb
@@ -1,9 +1,9 @@
-When(/^I create an Account Delegate$/) do
+When /^I create an Account Delegate$/ do
   @accountDelegate = create AccountDelegateObject
 end
 
-Then(/^the Approval From This Amount and Approval To This Amount fields should be blank$/) do
-  on (AccountDelegatePage) do |page|
+Then /^the Approval From This Amount and Approval To This Amount fields should be blank$/ do
+  on AccountDelegatePage do |page|
     page.approval_from_amount.nil?
     page.approval_to_amount.nil?
   end
