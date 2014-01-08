2468f371c9ca118422911ad81a2a6f86bb845a2a
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-01-08 11:26:55 -0500

    [KFSQA-551] Progress...

--- features/step_definitions/preencumbrance.rb
+++ features/step_definitions/preencumbrance.rb
@@ -1,13 +1,19 @@
 When /^I blanket approve a Pre-Encumbrance Document for Account number "([^"]*)"$/ do |acct_num|
-  @browser.goto "#{$base_url}portal.do?channelTitle=Pre-Encumbrance&channelUrl=financialPreEncumbrance.do?methodToCall=docHandler&command=initiate&docTypeName=PE"
-  on PreEncumbrancePage do |page|
-    page.description.set '[KFSQA-587] Test Account Pre-Encumbrance'
-    page.new_encumbrance.set {
-      :chart_code '',
-      :account_number '',
-      :object '',
-      :amount ''
+  visit PreEncumbrancePage do |page|
+    settings = {
+      description:    '[KFSQA-587] Test Account Pre-Encumbrance',
+      chart_code:     'IT',
+      account_number: acct_num,
+      object:         '6100',
+      amount:         '0.01'
     }
+
+
+    #page.description.set    '[KFSQA-587] Test Account Pre-Encumbrance'
+    #page.chart_code.set     'IT'
+    #page.account_number.set acct_num
+    #page.object.set         '6100'
+    #page.amount.set         '0.01'
   end
 end
 
