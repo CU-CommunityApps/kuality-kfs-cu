13e47fcf29a76e1b015bdb29cde905c26b9242a3
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-01-08 09:57:20 -0500

    [KFSQA-572] Fixed some style issues. Added a 10 second sleep before checking if the eDoc goes final to allow time for processing. This is nearly ready to commit - my only complaint would be that for some steps you use "Account Global eDoc" and for others you use "Account Global maintenance document". After merging the branch into master, but *before* pushing the merge to GitHub, please pick one or the other and update the appropriate steps and step definitions. Other than that, it is good to go.

--- features/chart_of_accounts/account_global.feature
+++ features/chart_of_accounts/account_global.feature
@@ -12,17 +12,15 @@ Feature: Global Account
 #    TODO:: Move this to a better location as more tests are written
 #  Feature: KFSQA-577 Account Global Lookup Modification
 #  As a KFS Chart Manager I want to add multiple account lines to the Account Global using Organizational Codes because this will save me time
- @wip
  Scenario: Create an Account Global using an organization hierarchy-577
     Given  I am logged in as a KFS Chart Manager
     And     I create an Account Global maintenance document with multiple accounting lines
-    When  I submit the document
+    When  I submit the Account Global maintenance document
     Then    The Account Global eDoc will become final
 
-@wip
-@KFSQA-572
+@KFSQA-572 @wip
   Scenario: Create Account Global eDoc with Major Reporting Category
-    Given   I am logged in as a KFS Chart Manager
-    And      I create an Account Global eDoc with an existing Major Reporting Category
-    When  I submit the document
-    Then    The Account Global eDoc will become final
+    Given I am logged in as a KFS Chart Manager
+    And   I create an Account Global eDoc with an existing Major Reporting Category
+    When  I submit the Account Global maintenance document
+    Then  The Account Global eDoc will become final
--- features/step_definitions/chart_of_accounts/account_global.rb
+++ features/step_definitions/chart_of_accounts/account_global.rb
@@ -17,23 +17,24 @@ Then /^I should see a list of Major Reporting Category Codes$/ do
   end
 end
 
-And(/^I create an Account Global maintenance document with multiple accounting lines$/) do
+And /^I create an Account Global maintenance document with multiple accounting lines$/ do
   @account_global = create AccountGlobalObject,
                            add_multiple_accounting_lines: 'yes',
                            search_account_number: '10007*'
 end
 
-When(/^I submit the document$/) do
+When /^I submit the Account Global maintenance document$/ do
   @account_global.submit
 end
 
-And(/^I create an Account Global eDoc with an existing Major Reporting Category$/) do
+And /^I create an Account Global eDoc with an existing Major Reporting Category$/ do
   @account_global = create AccountGlobalObject,
-                           major_reporting_category_code: 'FACULTY'
+                           major_reporting_category_code: 'FACULTY' # TODO: It would be nice if this could be obtained either through a search or a service, instead of being hard-coded.
 end
 
-Then(/^The Account Global eDoc will become final$/) do
-  on(AccountGlobalPage) do |page|
+Then /^The Account Global eDoc will become final$/ do
+  on AccountGlobalPage do |page|
+    sleep 10
     page.reload
     page.document_status.should == 'FINAL'
   end
