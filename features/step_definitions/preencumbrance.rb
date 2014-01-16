4e54bd92399c56f83fac6395fa8dfd5200a7d3ad
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-01-16 09:58:16 -0500

    [KFSQA-551] Just have to get the reference number somehow...

--- features/step_definitions/preencumbrance.rb
+++ features/step_definitions/preencumbrance.rb
@@ -9,8 +9,8 @@ Then /^the Pre-Encumbrance posts a GL Entry with one of the following statuses$/
   visit(MainPage).general_ledger_entry
   on GeneralLedgerEntryLookupPage do |page|
     # We'll assume that fiscal year and fiscal period default to nowish
-    page.chart_code.fit @preencumbrance.chart_code
-    page.account_number.fit @preencumbrance.account_number
+    page.chart_code.fit @preencumbrance.encumbrance_chart_code
+    page.account_number.fit @preencumbrance.encumbrance_account_number
     page.balance_type_code.fit ''
     page.pending_entry_approved_indicator_all
 
@@ -25,13 +25,26 @@ Then /^the Pre-Encumbrance posts a GL Entry with one of the following statuses$/
 end
 
 When /^I disencumber the Account$/ do
-  visit(MainPage).open_encumbrances
-  on OpenEncumbranceLookupPage do |page|
-    page.chart_code.fit 'IT'
-    page.account_number.fit @account_number
-    page.encumbrance_balance_type.fit 'PE'
+  visit(MainPage).general_ledger_entry
+  on GeneralLedgerEntryLookupPage do |page|
+    page.chart_code.fit @preencumbrance.encumbrance_chart_code
+    page.account_number.fit @preencumbrance.encumbrance_account_number
+    page.balance_type_code.fit ''
+    page.pending_entry_approved_indicator_all
+
     page.search
 
-# How do I actually disencumber one of these mothers?
+    # The description field gets truncated to 40 characters for this display. Joy.
+    truncated_description = @preencumbrance.description.length > 40 ? @preencumbrance.description[0,39] : @preencumbrance.description
+    page.open_item_via_text(truncated_description, @preencumbrance.document_id)
   end
+
+  on(PreEncumbrancePage) do |page|
+    page.accounting_lines.
+  end # Figure out how to actually grab the reference number...
+
+exit
+
+  # Note: You must have captured the account number of the random account in a previous step to use this step.
+  @preencumbrance = create PreEncumbranceObject, disencumbrance_account_number: @account_number, press: PreEncumbrancePage::BLANKET_APPROVE
 end
