c9582c99f71bfae66fafe598c3eaf30a76ac92d0
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-01-21 16:36:54 -0500

    Fixed some abbreviations.

--- features/step_definitions/chart_of_accounts/account.rb
+++ features/step_definitions/chart_of_accounts/account.rb
@@ -26,7 +26,7 @@ And /^I copy an Account$/ do
 end
 
 And /^I save an Account with a lower case Sub Fund Program$/ do
-  @account = create AccountObject, sub_fnd_group_cd: 'board', press: :save
+  @account = create AccountObject, sub_fnd_group_code: 'board', press: :save
 end
 
 Then /^the Account Maintenance Document goes to (.*)/ do |doc_status|
@@ -35,7 +35,7 @@ Then /^the Account Maintenance Document goes to (.*)/ do |doc_status|
 end
 
 When /^I submit an account with blank SubFund group Code$/ do
-  @account = create AccountObject, sub_fnd_group_cd: '', press: :submit
+  @account = create AccountObject, sub_fnd_group_code: '', press: :submit
 end
 
 Then /^I should get an error on saving that I left the SubFund Group Code field blank$/ do
@@ -86,7 +86,7 @@ end
 And /^I edit an Account with a Sub-Fund Group Code of (.*)/ do |sub_fund_group_code|
   visit(MainPage).account
   on AccountLookupPage do |page|
-    page.sub_fnd_group_cd.fit sub_fund_group_code
+    page.sub_fnd_group_code.fit sub_fund_group_code
     page.search
     page.edit_random
   end
@@ -126,20 +126,20 @@ When /^I save an Account document with only the ([^"]*) field populated$/ do |fi
       number:               random_alphanums(7),
       name:                 random_alphanums(10),
       org_code:               '01G0',
-      campus_cd:            'IT - Ithaca', #TODO grab this from config file
+      campus_code:            'IT - Ithaca', #TODO grab this from config file
       effective_date:       '01/01/2010',
-      postal_cd:            '14853', #TODO grab this from config file
+      postal_code:            '14853', #TODO grab this from config file
       city:                 'Ithaca', #TODO grab this from config file
       state:                'NY', #TODO grab this from config file
       address:              'Cornell University', #TODO grab this from config file
-      type_cd:              'CC - Contract College', #TODO grab this from config file
-      sub_fnd_group_cd:     'ADMSYS',
-      higher_ed_funct_cd:   '4000',
-      restricted_status_cd: 'U - Unrestricted',
+      type_code:              'CC - Contract College', #TODO grab this from config file
+      sub_fnd_group_code:     'ADMSYS',
+      higher_ed_funct_code:   '4000',
+      restricted_status_code: 'U - Unrestricted',
       fo_principal_name:    'dh273',
       supervisor_principal_name:  'ccs1',
       manager_principal_name: 'aap98',
-      budget_record_level_cd: 'C - Consolidation',
+      budget_record_level_code: 'C - Consolidation',
       sufficient_funds_code:    'C - Consolidation',
       expense_guideline_text: 'expense guideline text',
       income_guideline_txt:   'incomde guideline text',
@@ -186,7 +186,7 @@ And(/^I create an Account with an Appropriation Account Number of (.*) and Sub-F
 end
 
 And /^I enter Sub Fund Group Code of (.*)/ do |sub_fnd_group_cd|
-  on(AccountPage).sub_fnd_group_cd.set sub_fnd_group_cd
+  on(AccountPage).sub_fnd_group_code.set sub_fnd_group_cd
 end
 
 And /^I enter Sub Fund Program Code of (.*)/  do |subfund_program_code|
