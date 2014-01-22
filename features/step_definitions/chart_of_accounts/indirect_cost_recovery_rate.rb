3a49ab2db7dc5410aa71fce5e4fe180d7af31e12
Todd Plesniak <tplesniak@rsmart.com>
2014-01-22 14:21:16 -0500

    KFAQA-589 corrected visit to maintenance tab

--- features/step_definitions/chart_of_accounts/indirect_cost_recovery_rate.rb
+++ features/step_definitions/chart_of_accounts/indirect_cost_recovery_rate.rb
@@ -1,8 +1,7 @@
 When /^I lookup a Rate ID using an alpha\-numeric value in the Indirect Cost Recovery Rate table$/ do
 
   on MaintenancePage do |page|
-    page.maintenance_tab
-    page.indirect_cost_recovery_rate
+    visit(MaintenancePage).indirect_cost_recovery_rate
   end
 
   on IndirectCostRecoveryRateLookupPage do |page|
