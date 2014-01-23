cff6355ba1f5253dca1873dbcf81eaee823d0de4
Tony <tony@rsmart.com>
2014-01-23 09:19:29 -0500

    initial work on [KFSQA-609]

--- features/step_definitions/document_search.rb
+++ features/step_definitions/document_search.rb
@@ -6,6 +6,7 @@ end
 
 And /^I search for all (.*) documents$/ do |doc_type|
   on DocumentSearch do |page|
+    page.date_created_from.set '01/01/2014'
     page.document_type.set doc_type
     page.search
   end
@@ -15,13 +16,10 @@ And /^I copy a random (.*) document$/ do |doc_status|
   on DocumentSearch do |search|
     search.item_row(doc_status).should exist
     if search.item_row(doc_status).present?
-#      search.item_row(doc_status).td.link.click
-#      open_document(search.item_row(doc_status).td.link.text)
       @document_id = search.item_row(doc_status).td.link.text
       doc_search
     end
     on KFSBasePage do |page|
-
       page.copy_current_document
       page.description.fit 'AFT testing copy'
       page.save
