213b4ecba7eb8ef8b2c1656871c42b41114a7b3a
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-01-31 11:13:02 -0500

    Removed some unused steps.

--- features/step_definitions/chart_of_accounts/object_code_global.rb
+++ features/step_definitions/chart_of_accounts/object_code_global.rb
@@ -5,16 +5,3 @@ end
 And /^I enter an invalid CG Reporting Code of (.*)$/ do |invalid_code|
   on(ObjectCodeGlobalPage).cg_reporting_code.fit invalid_code
 end
-
-#Then /^The Object Code Global document status should be PROCESSED$/ do
-#  on DocumentSearchPage do |page|
-#    page.doc_search
-#    page.document_id_field.when_present.set @object_code_global.document_id
-#    page.search
-#    page.result_item('FINAL').should exist
-#  end
-#end
-
-When /^I Blanket Approve the Object Code Global document$/ do
-  on(ObjectCodeGlobalPage).blanket_approve
-end
