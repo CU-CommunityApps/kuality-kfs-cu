34d314a144a39c59f2b8194efb590cbfd469aac3
Todd Plesniak <tplesniak@rsmart.com>
2014-02-27 11:12:57 -0500

    KFSQA-647 corrected spelling error

--- features/step_definitions/financial_processing/general_ledger.rb
+++ features/step_definitions/financial_processing/general_ledger.rb
@@ -1,8 +1,8 @@
-When /^I perform a (.*) Lookup using account number (.*)$/ do |gl_baance_inquiry_lookup, account_number|
-  gl_baance_inquiry_lookup = gl_baance_inquiry_lookup.gsub!(' ', '_').downcase
-  visit(MainPage).send(gl_baance_inquiry_lookup)
-  puts gl_baance_inquiry_lookup
-  if gl_baance_inquiry_lookup == 'current_fund_balance'
+When /^I perform a (.*) Lookup using account number (.*)$/ do |gl_balance_inquiry_lookup, account_number|
+  gl_balance_inquiry_lookup = gl_balance_inquiry_lookup.gsub!(' ', '_').downcase
+  visit(MainPage).send(gl_balance_inquiry_lookup)
+  puts gl_balance_inquiry_lookup
+  if gl_balance_inquiry_lookup == 'current_fund_balance'
     on CurrentFundBalanceLookupPage do |page|
       page.chart_code.set 'IT'
       page.account_number.set account_number
