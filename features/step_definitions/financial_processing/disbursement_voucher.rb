091fbd4435963a2b3c735c5c9e6550fd40caa899
lshyu <lshyu@vivantech.com>
2014-03-06 16:05:34 -0500

    [KFSQA-709] added 'auto_populate' flag to control whether to populate payment information when create DV

--- features/step_definitions/financial_processing/disbursement_voucher.rb
+++ features/step_definitions/financial_processing/disbursement_voucher.rb
@@ -3,5 +3,8 @@ When /^I start an empty Disbursement Voucher document$/ do
 end
 
 When /^I save a Disbursement Voucher document with only the Description field populated$/ do
-  @disbursement_voucher = create DisbursementVoucherObject, {press: :save}
+  default_fields = {
+      auto_populate:               'N'
+  }
+  @disbursement_voucher = create DisbursementVoucherObject, default_fields.merge({press: :save})
 end
