f2540f1e458a873678c27a7489618bac9330fac8
lshyu <lshyu@vivantech.com>
2014-03-11 10:04:09 -0400

    [KFSQA-681] changes made based on crucible KFSQA-12 review comments.

--- features/financial_processing/disbursement_voucher.feature
+++ features/financial_processing/disbursement_voucher.feature
@@ -6,8 +6,10 @@ Feature: Disbursement Voucher Creation
   Scenario: KFS User Initiates and Submits a Disbursement Voucher document with Payment to Retiree
     Given I am logged in as a KFS User
     When  I start an empty Disbursement Voucher document
-    And   I search for Disbursement Voucher with Retiree map3 and Reason Code B
-    And   I retrieve only One Result
-    And   I add accounting line with AccountNumber 'G003704', ObjectCode '6100', Amount '23' to test
+    And   I add the only payee with Retiree map3 and Reason Code B to the Disbursement Voucher
+    And   I add an Accounting Line to the Disbursement Voucher with the following fields:
+      | Number       | G003704 |
+      | Object Code  | 6100  |
+      | Amount       | 23 |
     And   I submit the Disbursement Voucher document
     And   the Disbursement Voucher document goes to ENROUTE
--- features/step_definitions/financial_processing/disbursement_voucher.rb
+++ features/step_definitions/financial_processing/disbursement_voucher.rb
@@ -1,34 +1,26 @@
 When /^I start an empty Disbursement Voucher document$/ do
-  default_fields = {
-      payee_id:        nil, # not populating payment info when 'initialize'
-      press:           :save
-  }
-  @disbursement_voucher = create DisbursementVoucherObject, default_fields
+  @disbursement_voucher = create DisbursementVoucherObject, payee_id: nil
 end
 
-And /^I search for Disbursement Voucher with Retiree (\w+) and Reason Code (\w)$/ do |net_id, reason_code|
+And /^I add the only payee with Retiree (\w+) and Reason Code (\w+) to the Disbursement Voucher$/ do |net_id, reason_code|
   case reason_code
     when 'B'
       @disbursement_voucher.payment_reason_code = 'B - Reimbursement for Out-of-Pocket Expenses'
   end
   @disbursement_voucher.payee_id = net_id
   @disbursement_voucher.vendor_payee = false
-  @disbursement_voucher.payee_lookup
-
+  on (PaymentInformationTab) {|tab| @disbursement_voucher.fill_in_payment_info(tab)}
 end
 
-And /^I retrieve only One Result$/ do
-  @disbursement_voucher.num_result_rows.should == 2 # header+value rows
-end
-
-And /^I add accounting line with AccountNumber '(\w+)', ObjectCode '(\w+)', Amount '(\w+)' to test$/ do |account_number,object,amount|
-  # TODO : not sure if the description of this step is ok ?
-  @disbursement_voucher.check_amount = amount
+And /^I add an Accounting Line to the Disbursement Voucher with the following fields:$/ do |table|
+  accounting_line_info = table.rows_hash
+  accounting_line_info.delete_if { |k,v| v.empty? }
+  @disbursement_voucher.check_amount = accounting_line_info['Amount']
   @disbursement_voucher.change_default_check_amount
   @disbursement_voucher.add_source_line({
-                                            account_number: account_number,
-                                            object: object,
-                                            amount: amount
+                                            account_number: accounting_line_info['Number'],
+                                            object: accounting_line_info['Object Code'],
+                                            amount: accounting_line_info['Amount']
                                         })
 
 end
