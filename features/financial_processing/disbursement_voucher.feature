58bf36c5928637b7976d90a11ee537a0df6693f2
lshyu <lshyu@vivantech.com>
2014-03-11 15:04:33 -0400

    [KFSQA-709] Merged KFSQA-681 to this jira and reworked.

--- features/financial_processing/disbursement_voucher.feature
+++ features/financial_processing/disbursement_voucher.feature
@@ -1,10 +1,25 @@
 Feature: Disbursement Voucher Creation
 
+  [KFSQA-681] Retiree should get a DV; People with Multiple Affiliations in PeopleSoft should only return one row.
+
   [KFSQA-709] Because it saves time, I as a KFS User should be able to
   initiate a Disbursement Voucher document with just the description.
+
+  @KFSQA-681
+  Scenario: KFS User Initiates and Submits a Disbursement Voucher document with Payment to Retiree
+    Given I am logged in as a KFS User
+    When  I start an empty Disbursement Voucher document
+    And   I add the only payee with Retiree map3 and Reason Code B to the Disbursement Voucher
+    And   I add an Accounting Line to the Disbursement Voucher with the following fields:
+      | Number       | G003704 |
+      | Object Code  | 6100    |
+      | Amount       | 23      |
+    And   I submit the Disbursement Voucher document
+    And   the Disbursement Voucher document goes to ENROUTE
 
   @KFSQA-709 @wip
   Scenario: KFS User Initiates a Disbursement Voucher document with only a description field
     Given I am logged in as a KFS User
-    When  I save a Disbursement Voucher document with only the Description field populated
+    When  I start an empty Disbursement Voucher document with only the Description field populated
+    And   I save the Disbursement Voucher document
     Then  the Disbursement Voucher document goes to SAVED
--- features/step_definitions/financial_processing/disbursement_voucher.rb
+++ features/step_definitions/financial_processing/disbursement_voucher.rb
@@ -2,9 +2,30 @@ When /^I start an empty Disbursement Voucher document$/ do
   @disbursement_voucher = create DisbursementVoucherObject
 end
 
-When /^I save a Disbursement Voucher document with only the Description field populated$/ do
-  default_fields = {
-      auto_populate:               'N'
-  }
-  @disbursement_voucher = create DisbursementVoucherObject, default_fields.merge({press: :save})
+And /^I add the only payee with Retiree (\w+) and Reason Code (\w+) to the Disbursement Voucher$/ do |net_id, reason_code|
+  case reason_code
+    when 'B'
+      @disbursement_voucher.payment_reason_code = 'B - Reimbursement for Out-of-Pocket Expenses'
+  end
+  @disbursement_voucher.payee_id = net_id
+  @disbursement_voucher.vendor_payee = false
+  on (PaymentInformationTab) {|tab| @disbursement_voucher.fill_in_payment_info(tab)}
+end
+
+And /^I add an Accounting Line to the Disbursement Voucher with the following fields:$/ do |table|
+  accounting_line_info = table.rows_hash
+  accounting_line_info.delete_if { |k,v| v.empty? }
+  @disbursement_voucher.check_amount = accounting_line_info['Amount']
+  @disbursement_voucher.change_default_check_amount
+  @disbursement_voucher.add_source_line({
+                                            account_number: accounting_line_info['Number'],
+                                            object: accounting_line_info['Object Code'],
+                                            amount: accounting_line_info['Amount']
+                                        })
+
+end
+
+When /^I start an empty Disbursement Voucher document with only the Description field populated$/ do
+  # Currently 'description' is included in dataobject's default, so this step is just in case 'description' is not in default.
+  @disbursement_voucher = create DisbursementVoucherObject, description: random_alphanums(40, 'AFT')
 end
