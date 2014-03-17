24d88a4b1af36d7d661423eb3b4383718a180c5a
lshyu <lshyu@vivantech.com>
2014-03-17 15:53:39 -0400

    [KFSQA-700] remove code belongs to KFSQA-697

--- features/step_definitions/financial_processing/disbursement_voucher.rb
+++ features/step_definitions/financial_processing/disbursement_voucher.rb
@@ -67,25 +67,6 @@ And /^I search for the payee with Terminated Employee (\w+) and Reason Code (\w+
   end
 end
 
-And /^I copy a Disbursement Voucher document with Tax Address to persist$/ do
-  # save original address for comparison.  The address fields are readonly
-  old_address = []
-  on (PaymentInformationTab) { |tab|
-    old_address = [tab.address_1_value, tab.address_2_value.strip, tab.city_value, tab.state_value, tab.country_value, tab.postal_code_value]
-  }
-
-  get("disbursement_voucher").send("copy_current_document")
-
-  # validate the Tax Address is copied over
-  copied_address = []
-  on (PaymentInformationTab) { |tab|
-    copied_address = [tab.address_1.value, tab.address_2.value.strip, tab.city.value, tab.state.value, tab.country.selected_options.first.text, tab.postal_code.value]
-  }
-
-  puts "addresses", old_address, copied_address
-  old_address.should == copied_address
-end
-
 And /^I change the Check Amount for the Disbursement Voucher document to (.*)$/ do |amount|
   on (PaymentInformationTab) {|tab| tab.check_amount.fit amount}
   on (AccountingLine) {|line| line.update_source_amount(0).fit amount}
