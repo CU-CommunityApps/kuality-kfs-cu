dc354b2e1491ae1e217e3b9b22ee1bfa1f460985
lshyu <lshyu@vivantech.com>
2014-03-17 14:00:38 -0400

    [KFSQA-700] scripts to test that DV allows petty cash and check amount change

--- features/financial_processing/disbursement_voucher.feature
+++ features/financial_processing/disbursement_voucher.feature
@@ -8,6 +8,8 @@ Feature: Disbursement Voucher Creation
 
   [KFSQA-685] Active Staff, Former Student, and Alumnus should get a DV; People with Multiple Affiliations in PepleSoft should only return one row.
 
+  [KFSQA-700] Allow usage of Revolving Fund (Petty Cash) DV Payment Types.
+
   @KFSQA-681 @smoke @sloth
   Scenario: KFS User Initiates and Submits a Disbursement Voucher document with Payment to Retiree
     Given I am logged in as a KFS User
@@ -52,4 +54,23 @@ Feature: Disbursement Voucher Creation
       | Description  | Line Test Number 1 |
     And   I submit the Disbursement Voucher document
     And   the Disbursement Voucher document goes to ENROUTE
+
+  @KFSQA-700 @tortoise @wip
+  Scenario: Disbursement Voucher document allow usage of Revolving Fund (Petty Cash) Payment Types
+    Given I am logged in as "rlc56"
+    When  I start an empty Disbursement Voucher document with Payment to Vendor 21541-0 with Single Address and Reason Code K
+    And   I add an Accounting Line to the Disbursement Voucher with the following fields:
+      | Number       | G003704        |
+      | Object Code  | 6540           |
+      | Amount       | 10             |
+      | Description  | DV12 Test....  |
+    And   I save the Disbursement Voucher document
+    And   I view the Disbursement Voucher document
+    And   I change the Check Amount for the Disbursement Voucher document to 22.22
+    And   I submit the Disbursement Voucher document
+    And   the Disbursement Voucher document goes to ENROUTE
+    And   I am logged in as "djj1"
+    And   I view the Disbursement Voucher document
+    And   I approve the Disbursement Voucher document
+    And   the Disbursement Voucher document goes to FINAL
 
--- features/step_definitions/financial_processing/disbursement_voucher.rb
+++ features/step_definitions/financial_processing/disbursement_voucher.rb
@@ -2,8 +2,23 @@ When /^I start an empty Disbursement Voucher document$/ do
   @disbursement_voucher = create DisbursementVoucherObject
 end
 
-When /^I start an empty Disbursement Voucher document with Payment to Vendor (.*)$/ do |vendor_number|
-  @disbursement_voucher = create DisbursementVoucherObject, payee_id: vendor_number
+When /^I start an empty Disbursement Voucher document with Payment to Vendor (\d+-\d+) with (\w+) Address and Reason Code (\w+)$/ do |vendor_number, address_type, reason_code|
+  case reason_code
+    when 'B'
+      payment_reason = 'B - Reimbursement for Out-of-Pocket Expenses'
+    when 'K'
+      payment_reason = 'K - Univ PettyCash Custodian Replenishment'
+
+  end
+  case address_type
+    when 'Single' # will not do address lookup after payee look
+      address_type_description =  nil
+    when 'Tax'
+      address_type_description = 'TX - TAX'
+    when 'Remit'
+      address_type_description = 'RM - REMIT'
+  end
+  @disbursement_voucher = create DisbursementVoucherObject, payee_id: vendor_number, payment_reason_code: payment_reason, address_type_description: address_type_description
 end
 
 
@@ -28,8 +43,7 @@ end
 And /^I add an Accounting Line to the Disbursement Voucher with the following fields:$/ do |table|
   accounting_line_info = table.rows_hash
   accounting_line_info.delete_if { |k,v| v.empty? }
-  @disbursement_voucher.check_amount = accounting_line_info['Amount']
-  @disbursement_voucher.change_default_check_amount
+  on (PaymentInformationTab) {|tab| tab.check_amount.fit accounting_line_info['Amount']}
   @disbursement_voucher.add_source_line({
                                             account_number: accounting_line_info['Number'],
                                             object: accounting_line_info['Object Code'],
@@ -51,4 +65,28 @@ And /^I search for the payee with Terminated Employee (\w+) and Reason Code (\w+
     plookup.search
     plookup.frm.divs(id: 'lookup')[0].parent.text.should include 'No values match this search'
   end
+end
+
+And /^I copy a Disbursement Voucher document with Tax Address to persist$/ do
+  # save original address for comparison.  The address fields are readonly
+  old_address = []
+  on (PaymentInformationTab) { |tab|
+    old_address = [tab.address_1_value, tab.address_2_value.strip, tab.city_value, tab.state_value, tab.country_value, tab.postal_code_value]
+  }
+
+  get("disbursement_voucher").send("copy_current_document")
+
+  # validate the Tax Address is copied over
+  copied_address = []
+  on (PaymentInformationTab) { |tab|
+    copied_address = [tab.address_1.value, tab.address_2.value.strip, tab.city.value, tab.state.value, tab.country.selected_options.first.text, tab.postal_code.value]
+  }
+
+  puts "addresses", old_address, copied_address
+  old_address.should == copied_address
+end
+
+And /^I change the Check Amount for the Disbursement Voucher document to (.*)$/ do |amount|
+  on (PaymentInformationTab) {|tab| tab.check_amount.fit amount}
+  on (AccountingLine) {|line| line.update_source_amount(0).fit amount}
 end
