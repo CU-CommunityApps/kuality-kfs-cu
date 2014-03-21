4116583a438f21df0d78e4bbf1429a9666c3187c
sbower <shawn.bower@gmail.com>
2014-03-21 10:19:41 -0400

    adding webservice support

--- features/step_definitions/login_to_KFS.rb
+++ features/step_definitions/login_to_KFS.rb
@@ -27,6 +27,7 @@ Given /^I am logged in as a KFS Chart Administrator$/ do
 end
 
 Given /^I am logged in as a KFS Chart Manager$/ do
+  #visit(BackdoorLoginPage).login_as(get_first_principal_name_for_role('KFS-SYS', 'Chart Manager'))
   visit(BackdoorLoginPage).login_as('dh273') #TODO get from role service
 end
 
---
+++ features/support/global_config.rb
@@ -0,0 +1,32 @@
+module GlobalConfig
+  def global_config
+    @@global_config ||= YAML.load_file("#{File.dirname(__FILE__)}/config.yml")[:basic]
+  end
+  def ksb_client
+    #sub string to remove trailing 
+    @@ksb_client ||= KSBServiceClient.new("#{File.dirname(__FILE__)}/client-sign.properties", "cynergy-dev", global_config[:rice_url].sub(/(\/)+$/,''))
+  end
+  def role_service
+    @@role_service ||= ksb_client.getRoleService()
+  end
+  def identity_service
+    @@identity_service ||= ksb_client.getIdentityService()
+  end
+  def get_first_principal_id_for_role(name_space, role_name)
+    role_service.getRoleMemberPrincipalIds(name_space, role_name, StringMapEntryListType.new).getPrincipalId().get(0)
+  end
+  def get_principal_name_for_principal_id(principal_name)
+    identity_service.getEntityByPrincipalId(principal_name).getPrincipals().getPrincipal().get(0).getPrincipalName()
+  end
+  def get_first_principal_name_for_role(name_space, role_name)
+    @@prinicpal_names ||= Hash.new{|hash, key| hash[key] = Hash.new}
+    
+    if !@@prinicpal_names[name_space][role_name].nil?
+      @@prinicpal_names[name_space][role_name]
+    else
+      @@prinicpal_names[name_space][role_name] = get_principal_name_for_principal_id(get_first_principal_id_for_role(name_space, role_name))
+    end
+  end
+end
+
+World(GlobalConfig)
