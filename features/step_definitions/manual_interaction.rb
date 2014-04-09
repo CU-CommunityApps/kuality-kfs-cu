4f38c0c1e3ec32456332be33049989fd2b93f91c
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-04-09 15:57:15 -0400

    [KFSQA-644] Small update. We are probably going to drop this actual JIRA, but hopefully we can still merge in the non-Feature work.

--- features/step_definitions/manual_interaction.rb
+++ features/step_definitions/manual_interaction.rb
@@ -1,18 +1,12 @@
 And /^I press the (.*) key$/ do |key|
-  puts "I press the #{key} key:"
-  puts key.inspect
-  puts @browser.inspect
   @browser.send_keys snake_case(key)
 end
 
 Then /^my cursor is on the (.*) button$/ do |button|
-  puts "my cursor is on the #{button} button:"
-  puts @browser.frm.input(title: button).inspect
   @browser.frm.input(title: button).focused?
 end
 
 Then /^my cursor is on the (.*) field$/ do |title|
-  puts "my cursor is on the #{title} field:"
   case title
     when 'Attach File'
       puts @browser.frm.input(id: 'attachmentFile').inspect
@@ -21,7 +15,5 @@ Then /^my cursor is on the (.*) field$/ do |title|
 end
 
 When /^I click the (.*) button$/ do |button|
-  puts "I click the #{button} button:"
-  puts @browser.frm.input(title: button).inspect
   @browser.frm.input(title: button).click
 end
