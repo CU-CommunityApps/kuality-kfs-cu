dca804617f1957da8a5a278862b608e843579daa
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-04-21 09:37:37 -0400

    Fixed a step that was broken for no super obvious reason.

--- features/step_definitions/errors.rb
+++ features/step_definitions/errors.rb
@@ -30,5 +30,5 @@ Then /^I should get an error that starts with "([^"]*)"$/ do |error_msg|
 end
 
 And /^I should get an Authorization Exception Report error$/ do
-  @browser.frm.div(id: 'headerarea').h1.text.rstrip.should == 'Authorization Exception Report'
+  $current_page.frm.div(id: 'headerarea').h1.text.rstrip.should == 'Authorization Exception Report'
 end
