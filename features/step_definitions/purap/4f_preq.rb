f7ad4512c76fa59a967f9e5b16f03b35ce5dd5e1
lshyu <lshyu@vivantech.com>
2014-04-22 13:39:43 -0400

    [KFSQA-745] uncomment add file

--- features/step_definitions/purap/4f_preq.rb
+++ features/step_definitions/purap/4f_preq.rb
@@ -56,7 +56,7 @@ And /^I add an Attachment to the Requisition document$/ do
   on RequisitionPage do |page|
     page.note_text.fit random_alphanums(40, 'AFT-NoteText')
     page.send_to_vendor.fit 'Yes'
-    #page.attach_notes_file.set($file_folder+@requisition.attachment_file_name)
+    page.attach_notes_file.set($file_folder+@requisition.attachment_file_name)
     page.add_note
     page.attach_notes_file_1.should exist #verify that note is indeed added
 
