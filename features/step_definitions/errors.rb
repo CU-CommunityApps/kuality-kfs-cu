e3733b3e74211f42ba96ab5181ff958f8bec518b
Todd Plesniak <tplesniak@rsmart.com>
2014-05-06 08:59:14 -0700

    KFSPRJQ-307 low case T for steps

--- features/step_definitions/errors.rb
+++ features/step_definitions/errors.rb
@@ -8,16 +8,16 @@ Then /^an error should say (.*)$/ do |error|
   $current_page.errors.should include errors[error]
 end
 
-Then /^The document should have no errors$/ do
+Then /^the document should have no errors$/ do
   $current_page.errors.should == []
 end
 
-Then /^The document should save successfully$/ do
+Then /^the document should save successfully$/ do
   $current_page.left_errmsg_text.should include 'Document was successfully saved.'
   $current_page.document_status.should == 'SAVED'
 end
 
-Then /^The document should submit successfully$/ do
+Then /^the document should submit successfully$/ do
   $current_page.left_errmsg_text.should include 'Document was successfully submitted.'
 end
 
