057f37031c5115a43aa1ed1bf120a7b1efe8a406
lshyu <lshyu@vivantech.com>
2014-05-19 10:13:21 -0400

    [KFSQA-985] scripts to run labor nightly batch jobs

--- features/step_definitions/labor_distribution/labor_distribution.rb
+++ features/step_definitions/labor_distribution/labor_distribution.rb
@@ -27,3 +27,33 @@ When /^I start an empty Salary Expense Transfer document$/ do
   @salary_expense_transfer = create SalaryExpenseTransferObject
 end
 
+
+Then /^I RUN THE NIGHTLY LABOR BATCH PROCESSES $/ do
+  steps %Q{
+    Given I am logged in as a KFS Operations
+    And I run the Labor Enterprise Feed Process
+    And I run the Labor Nightly Out Process
+    And I run the Labor Scrubber Process
+    And I run the Labor Poster Process
+    And I run the Labor Balancing Job
+    And I run the Labor Feed Job
+    And I run the Labor Clear Pending Entries Job   }
+
+  # GL nightly is deferred; See QA-830
+  #step "I run the GL Nightly Processes"
+end
+
+And /^I run the GL Nightly Processes$/ do
+  # generate output files batch jobs
+  steps %Q{
+    And   I generate the ACH XML File
+    And   I generate the Check XML File
+    And   I generate the Cancelled Check XML File
+    And   I send EMAIL Notices to ACH Payees
+    And   I process Cancels and Paids
+    And   I generate the GL Files from PDP
+    And   I populate the ACH Bank Table
+    And   I clear out PDP Temporary Tables
+ }
+
+end
