f5d15ab7e89767f4036e99268caf3caedb35dd44
lshyu <lshyu@vivantech.com>
2014-06-18 12:38:30 -0400

    [KFSQA-1006] Scripts to test change an Existing Expense Using a Distribution of Income and Expense to Create a Capital Asset

--- features/financial_processing/distribution_of_income_and_expense.feature
+++ features/financial_processing/distribution_of_income_and_expense.feature
@@ -2,6 +2,8 @@ Feature: Distribution of Income and Expense
 
   [KFAQA-648] I want to create a DI with an Accounting Line Change by an FO
 
+  [KFSQA-1006] I credit an expenditure from a source account and create an asset into the target account.
+
   @KFAQA-648 @Approving @DI @Edit @smoke @sloth @needs-clean-up @broken! @pending
   Scenario: Reject Approver Account changes unless they are the Fiscal Officer
     Given   I am logged in as a KFS User
@@ -24,3 +26,26 @@ Feature: Distribution of Income and Expense
     And     I approve the Distribution Of Income And Expense document
     When    I view the Distribution Of Income And Expense document
     Then    the Notes and Attachment Tab says "Accounting Line changed from"
+
+  @KFSQA-1006 @DI @E2E @slug @cornell @wip
+  Scenario: I credit an expenditure from a source account and create an asset into the target account.
+    Given   I am logged in as a KFS User for the DI document
+    And     I start an empty Distribution Of Income And Expense document
+    And     I add a Source Accounting Line to the Distribution Of Income And Expense document with the following:
+      | Chart Code   | IT      |
+      | Number       | 1003005 |
+      | Object Code  | 6540    |
+      | Amount       | 8000    |
+    And     I add a Target Accounting Line to the Distribution Of Income And Expense document with the following:
+      | Chart Code   | IT      |
+      | Number       | 1003010 |
+      | Object Code  | 3630    |
+      | Amount       | 8000    |
+    And     I select accounting line and create Capital Asset
+    And     I distribute Capital Asset amount
+    And     I add a tag and location for Capital Asset
+    And     I submit the Distribution Of Income And Expense document
+    And     the Distribution Of Income And Expense document goes to ENROUTE
+    And     I route the Distribution Of Income And Expense document to final
+    And     I run the nightly Capital Asset jobs
+    And     I build a Capital Asset from the General Ledger
--- features/step_definitions/financial_processing/distribution_of_income_and_expense.rb
+++ features/step_definitions/financial_processing/distribution_of_income_and_expense.rb
@@ -30,3 +30,71 @@ And /^I add a (From|To) amount of "(.*)" for account "(.*)" with object code "(.
     end
   end
 end
+
+And /^I select accounting line and create Capital Asset$/ do
+  on DistributionOfIncomeAndExpensePage do |page|
+    page.accounting_lines_for_capitalization_select.set
+    page.distribution_method.fit 'Distribute cost by amount'
+    page.create_asset
+  end
+end
+
+And /^I distribute Capital Asset amount$/ do
+  on DistributionOfIncomeAndExpensePage do |page|
+    @asset_account_number = page.asset_account_number
+    page.capital_asset_line_amount.fit page.remain_asset_amount
+    page.redistribute_amount
+  end
+end
+
+And /^I add a tag and location for Capital Asset$/ do
+  on(DistributionOfIncomeAndExpensePage).vendor_search
+  vendor_num = get_aft_parameter_value(ParameterConstants::REQS_NONB2B_VENDOR)
+
+  on VendorLookupPage do |page|
+    page.vendor_number.wait_until_present
+    page.vendor_number.fit vendor_num
+    page.search
+    page.return_value(vendor_num)
+  end
+  on DistributionOfIncomeAndExpensePage do |page|
+    page.capital_asset_qty.fit '1'
+    page.capital_asset_type.fit '019'
+    page.capital_asset_manufacturer.fit 'CA manufacturer'
+    page.capital_asset_description.fit random_alphanums(40, 'AFT')
+    page.insert_tag
+
+    page.use_new_tab
+    page.close_parents
+    page.tag_number.fit random_alphanums(8, 'T')
+    page.capital_asset_campus.fit 'IT - Ithaca'
+    page.capital_asset_building.fit '7000'
+    page.capital_asset_room.fit 'XXXXXXXX'
+  end
+end
+
+And /^I build a Capital Asset from the General Ledger$/ do
+  steps %Q{
+    Given I Login as an Asset Processor
+    And   I lookup a Capital Asset from GL transaction to process
+    And   I create asset from General Ledger Processing
+    And   I submit the Asset Global document
+    Then  the Asset Global document goes to FINAL
+   }
+end
+
+And /^I lookup a Capital Asset from GL transaction to process$/ do
+  visit(MainPage).capital_asset_builder_gl_transactions
+  on CabGlLookupPage do |page|
+    page.account_number.fit @asset_account_number
+    page.search
+    page.process(@asset_account_number)
+  end
+
+  on(CapitalAssetInformationProcessingPage).process
+end
+
+And /^I create asset from General Ledger Processing$/ do
+  on(GeneralLedgerProcessingPage).create_asset
+  @asset_global = create AssetGlobalObject
+end
