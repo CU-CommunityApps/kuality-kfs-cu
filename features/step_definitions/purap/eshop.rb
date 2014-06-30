e45ff8aca7aab551a0857d3d766eb0579a8a348c
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-06-30 11:07:50 -0400

    [KFSQA-731] Refactored the e-SHOP pages a little bit to make them a little more explicit.

--- features/step_definitions/purap/eshop.rb
+++ features/step_definitions/purap/eshop.rb
@@ -1,5 +1,5 @@
 And /^I search for an e\-SHOP item with a Non\-Sensitive Commodity Code$/ do
-  on ShopCatalogPage do |page|
+  on EShopCatalogPage do |page|
     page.choose_hosted_supplier                         'Staples'
     page.hosted_supplier_item_search_box('Staples').fit 'Paper, PASTELS 8.5X11 BLUE PAPER RM'
     page.hosted_supplier_item_search                    'Staples'
