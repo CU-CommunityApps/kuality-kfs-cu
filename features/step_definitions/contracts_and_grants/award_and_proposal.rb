aa9245d697fb5952e22a8f3cd58899fe2a1147a1
lshyu <lshyu@vivantech.com>
2014-08-25 11:20:06 -0400

    [KFSQA-977] some more review changes

--- features/step_definitions/contracts_and_grants/award_and_proposal.rb
+++ features/step_definitions/contracts_and_grants/award_and_proposal.rb
@@ -91,44 +91,48 @@ end
 
 And /^I add an existing (account|organization|project director) to the Award Document$/ do |coll_type|
   # Award must have at least one account.  organization/project director are optional
-  on AwardPage do |page|
-    case coll_type
-      when 'account'
+  case coll_type
+    when 'account'
+      on AwardPage do |page|
         @award.add_award_account_line({
                                           chart_code:               page.old_account_chart_code,
                                           principal_name:           page.old_account_project_director_principal_name,
                                           account_number:           page.old_account_number,
                                           active:                   :set
                                       })
-      when 'organization'
-        if page.current_organization_count.to_i > 0
-            @award.add_organization_line({
-                                             chart_code:                  page.old_organization_chart_code,
-                                             organization_code:           page.old_organization_code,
-                                             active:                      :set
-                                         })
+      end
+    when 'organization'
+      on OrganizationsTab do |tab|
+        if tab.current_organization_count.to_i > 0
+          @award.add_organization_line({
+                                           chart_code:                  tab.old_organization_chart_code,
+                                           organization_code:           tab.old_organization_code,
+                                           active:                      :set
+                                       })
         else
           # if there is no organization exists, then add 2 identicals
           new_chart_code  = get_aft_parameter_value(ParameterConstants::DEFAULT_CHART_CODE_WITH_NAME),
-          new_organization_code = fetch_random_department_organization_code,
+              new_organization_code = fetch_random_department_organization_code,
 
-          @award.add_organization_line({
-                                           chart_code:                   new_chart_code,
-                                           organization_code:            new_organization_code,
-                                           active:                       :set
-                                       })
+              @award.add_organization_line({
+                                               chart_code:                   new_chart_code,
+                                               organization_code:            new_organization_code,
+                                               active:                       :set
+                                           })
           @award.add_organization_line({
                                            chart_code:                   new_chart_code,
                                            organization_code:            new_organization_code,
                                            active:                       :set
                                        })
         end
-      when 'project director'
-        if page.current_project_director_count.to_i > 0
-            @award.add_project_director_line({
-                                                 principal_name:           page.old_project_director_principal_name,
-                                                 active:                   :set
-                                             })
+      end
+    when 'project director'
+      on ProjectDirectorsTab do |tab|
+        if tab.current_project_director_count.to_i > 0
+          @award.add_project_director_line({
+                                               principal_name:           tab.old_project_director_principal_name,
+                                               active:                   :set
+                                           })
         else
           # if there is no project director exists, then add 2 identicals
           new_principal_name = get_random_principal_name_for_role('KFS-SYS', 'Contracts & Grants Project Director')
@@ -142,43 +146,45 @@ And /^I add an existing (account|organization|project director) to the Award Doc
                                            })
 
         end
-    end
-
+      end
   end
 end
 
 
 Then /^I should get (account|organization|project director) exists error$/  do |coll_type|
-  on AwardPage do |page|
-    case coll_type
-      when 'account'
+  case coll_type
+    when 'account'
+      on AwardPage do |page|
         chart_code = page.old_account_chart_code[0..1]
         account_number = page.old_account_number
         step "I should get an error saying \"Account #{chart_code}-#{account_number} already exists on this Award\""
-      when 'organization'
-        chart_code = page.result_organization_chart_code[0..1]
-        organization_code = page.result_organization_code
-        step "I should get an error saying \"Organization #{chart_code}-#{organization_code} already exists on this Award\""
-      when 'project director'
-        pid = identity_service.getEntityByPrincipalName(page.update_project_director_principal_name.value).getPrincipals().getPrincipal().get(0).getPrincipalId()
-        step "I should get an error saying \"Project Director #{pid} already exists on this Award\""
-    end
-
+      end
+    when 'organization'
+      on OrganizationsTab do |tab|
+        chart_code = tab.result_organization_chart_code[0..1]
+        organization_code = tab.result_organization_code
+        on(AwardPage).errors.should include "Organization #{chart_code}-#{organization_code} already exists on this Award"
+        # TODO : following step is not working because $current_page is OrganizationsTab which does not have errors
+        #step "I should get an error saying \"Organization #{chart_code}-#{organization_code} already exists on this Award\""
+      end
+    when 'project director'
+      pid = identity_service.getEntityByPrincipalName(on(ProjectDirectorsTab).update_project_director_principal_name.value).getPrincipals().getPrincipal().get(0).getPrincipalId()
+      on(AwardPage).errors.should include "Project Director #{pid} already exists on this Award"
+      #step "I should get an error saying \"Project Director #{pid} already exists on this Award\""
   end
+
 
 end
 
 And /^I delete duplicate (account|organization|project director) from the Award document$/ do |coll_type|
-  on AwardPage do |page|
-    case coll_type
-      when 'account'
-        page.delete_account(page.current_account_count.to_i - 1)
-      when 'organization'
-        page.delete_organization(page.current_organization_count.to_i - 1)
-      when 'project director'
-        page.delete_project_director(page.current_project_director_count.to_i - 1)
-    end
-    end
+  case coll_type
+    when 'account'
+      on(AwardPage).delete_account(on(AwardPage).current_account_count.to_i - 1)
+    when 'organization'
+      on(OrganizationsTab).delete_organization(on(OrganizationsTab).current_organization_count.to_i - 1)
+    when 'project director'
+      on(ProjectDirectorsTab).delete_project_director(on(ProjectDirectorsTab).current_project_director_count.to_i - 1)
+  end
 end
 
 
