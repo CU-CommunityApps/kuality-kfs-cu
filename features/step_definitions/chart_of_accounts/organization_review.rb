4cf3b3fd1e609bd8ec09421f187070f0263ab6a6
Tony <tony@rsmart.com>
2014-08-28 13:07:42 -0400

    Fixed broken test [KFSQA-1077]

--- features/step_definitions/chart_of_accounts/organization_review.rb
+++ features/step_definitions/chart_of_accounts/organization_review.rb
@@ -4,8 +4,8 @@ And /^I edit an Organization Review$/ do
     page.search
     page.edit_random
   end
-  on OrganizationReviewRolePage do |page|
-    @organization_review_role = make OrganizationReviewRoleObject
+  on OrganizationReviewPage do |page|
+    @organization_review_role = make OrganizationReviewObject
     page.description.set random_alphanums(40, 'AFT')
     @organization_review_role.document_id = page.document_id
     page.save
