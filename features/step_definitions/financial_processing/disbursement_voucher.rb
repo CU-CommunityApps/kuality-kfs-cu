e1a7a6a3957547a369970a7e438fe623c97c0d66
lshyu <lshyu@vivantech.com>
2014-09-17 16:25:24 -0400

    [KFSQA-1007] merged from master

--- features/step_definitions/financial_processing/disbursement_voucher.rb
+++ features/step_definitions/financial_processing/disbursement_voucher.rb
@@ -116,28 +116,3 @@ Then /^the Payee Name should match$/ do
   on(PaymentInformationTab).payee_name.should == payee_name
 end
 
-And /^I add a tag and location for Capital Asset$/ do
-  on(DistributionOfIncomeAndExpensePage).vendor_search
-  vendor_num = get_aft_parameter_value(ParameterConstants::REQS_NONB2B_VENDOR)
-
-  on VendorLookupPage do |page|
-    page.vendor_number.wait_until_present
-    page.vendor_number.fit vendor_num
-    page.search
-    page.return_value(vendor_num)
-  end
-  on DistributionOfIncomeAndExpensePage do |page|
-    page.capital_asset_qty.fit '1'
-    page.capital_asset_type.fit '019'
-    page.capital_asset_manufacturer.fit 'CA manufacturer'
-    page.capital_asset_description.fit random_alphanums(40, 'AFT')
-    page.insert_tag
-
-    page.use_new_tab
-    page.close_parents
-    page.tag_number.fit random_alphanums(8, 'T')
-    page.capital_asset_campus.fit get_aft_parameter_value(ParameterConstants::DEFAULT_CAMPUS_CODE)
-    page.capital_asset_building.fit '7000'
-    page.capital_asset_room.fit 'XXXXXXXX'
-  end
-end
