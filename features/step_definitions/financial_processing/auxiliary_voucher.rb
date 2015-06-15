059c4db017ada22c55f90223f80c31e7aafba8dd
Nancy Kimble <nkk4@cornell.edu>
2015-06-15 13:47:22 -0400

    [KFSQA-1129] The hardcoded object code values for AFT KFSQA-627 that were also replaced by the WebService call to obtain a random object code value needed to be adjusted to exclude the parameter values listed in parmeters OBJECT_SUB_TYPES and OBJECT_LEVELS for KFS-FP.

--- features/step_definitions/financial_processing/auxiliary_voucher.rb
+++ features/step_definitions/financial_processing/auxiliary_voucher.rb
@@ -19,18 +19,31 @@ And /^I add credit and debit accounting lines with two different sub funds$/ do
     target_account = get_random_account
   end
 
-  object_code = get_random_object_code #using the same object code value for both source and target accounting lines
+
+  # We will be using the same object code value for both source and target accounting lines
+  # Object code value cannot have an object sub-type listed in the following system parameter:
+  #   namespace = KFS-FP    component = AuxiliaryVoucher   parameter name = OBJECT_SUB_TYPES
+  # Object code value cannot have an object level listed in the following system parameter:
+  #   namespace = KFS-FP    component = AuxiliaryVoucher   parameter name = OBJECT_LEVELS
+  object_sub_type_exclusions = get_parameter_values('KFS-FP', 'OBJECT_SUB_TYPES', component_code='AuxiliaryVoucher')
+  object_level_exclusions = get_parameter_values('KFS-FP', 'OBJECT_LEVELS', component_code='AuxiliaryVoucher')
+  object_code_object = nil
+
+  while true
+    object_code_object = get_random_object_code_object
+    break unless (object_sub_type_exclusions.include?(object_code_object['financialObjectSubTypeCode'][0]) & object_level_exclusions.include?(object_code_object['financialObjectLevelCode'][0]))
+  end
 
   on AuxiliaryVoucherPage do |page|
     @auxiliary_voucher.add_source_line({
                                            account_number:   source_account['accountNumber'][0],
-                                           object:           object_code,
+                                           object:           object_code_object['financialObjectCode'][0],
                                            credit:           '100',
                                            line_description: 'AV sample credit line'
                                        })
     @auxiliary_voucher.add_source_line({
                                            account_number:   target_account['accountNumber'][0],
-                                           object:           object_code,
+                                           object:           object_code_object['financialObjectCode'][0],
                                            debit:            '100',
                                            line_description: 'AV sample debit line'
                                        })
